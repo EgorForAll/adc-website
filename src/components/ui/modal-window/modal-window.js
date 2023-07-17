0419e6546bc77bff5b1e16ee652f6dc0720fde47
EgorForAll <eia.web.ss@gmail.com>
2023-07-17 12:20:47 +0300

    checkboxes has added

--- src/components/ui/modal-window/modal-window.js
+++ src/components/ui/modal-window/modal-window.js
@@ -39,18 +39,13 @@ const ModalWindow = ({parent, root, service}) => {
         <input id="telephone" name="telephone" className="modal-window__input phone" type="tel" ref={phoneRef}/>
         <label htmlFor="email" className="modal__window-subtitle">Email:</label>
         <input id="email" name="email" className="modal-window__input email" type="email"/>
-        <label className="modal__window-subtitle" htmlFor="type">Выберите услугу</label>
-        <select defaultValue={service} className="modal-window__input"  id="type">
-          <option value={'defualt'}>-</option>
+        <h4 className="modal__window-subtitle--services" htmlFor="type">Выберите услугу:</h4>
           {catalogList.map((item) =>
-            <option
-              key={catalogList.indexOf(item)}
-                value={item.name}
-                  selected={selected(item)}
-                  >
-                    {item.name}
-            </option>)}
-        </select>
+            <div className="modal-window__services-wrapper">
+             <input type="checkbox" name={item.name} id={item.id} className="modal-window__input--checkbox"/>
+            <label htmlFor={item.name} className="modal__window-subtitle--checkbox">{item.name}</label>
+            </div>
+          )}
         <div className="modal-window__time-wrapper">
           <label className="modal__window-subtitle" htmlFor="date">Выберите удобное для вас время</label>
           <input id="date" className="modal-window__input date" name="date" type="date"/>
